5b5e854b310855e837b182b5d4e95ad2fc7f07df
Divy <divy1211.dc@gmail.com>
2025-12-29 22:39:36 +0530

    Update names2

--- src/smx/sections/smx_frame/layer_header.py
+++ src/smx/sections/smx_frame/layer_header.py
@@ -10,10 +10,10 @@ class SmxLayerHeader(BaseStruct):
     __default_ver__ = DE_LATEST
 
     # @formatter:off
-    width: int =     Retriever(u16,  default = 0)
-    height: int =    Retriever(u16,  default = 0)
-    anchor_x: int =  Retriever(i16,  default = 0)
-    anchor_y: int =  Retriever(i16,  default = 0)
-    num_bytes: int = Retriever(u32,  default = 0)
-    unknown: int =   Retriever(u32,  default = 0)
+    width: int =                  Retriever(u16,  default = 0)
+    height: int =                 Retriever(u16,  default = 0)
+    anchor_x: int =               Retriever(i16,  default = 0)
+    anchor_y: int =               Retriever(i16,  default = 0)
+    num_bytes: int =              Retriever(u32,  default = 0)
+    num_bytes_uncompressed: int = Retriever(u32,  default = 0)
     # @formatter:on
